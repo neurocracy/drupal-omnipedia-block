46220be68233b7e6a1273f6c1a9712f6443b817c
Matei Stanca <i@ambientimpact.com>
2020-12-25 12:54:03 -0500

    Normalized all plug-in annotation white-space for ease of reading.

--- src/Plugin/AmbientImpact/Component/FounderMessage.php
+++ src/Plugin/AmbientImpact/Component/FounderMessage.php
@@ -8,9 +8,9 @@ use Drupal\ambientimpact_core\ComponentBase;
  * Founder message block component.
  *
  * @Component(
- *   id = "founder_message",
- *   title = @Translation("Founder message"),
- *   description = @Translation("A message from Omnipedia's founder.")
+ *   id           = "founder_message",
+ *   title        = @Translation("Founder message"),
+ *   description  = @Translation("A message from Omnipedia's founder.")
  * )
  */
 class FounderMessage extends ComponentBase {
