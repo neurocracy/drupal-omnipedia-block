5390f3e5ae64361937221b50e9e3c1f1aa6d9507
Matei Stanca <i@ambientimpact.com>
2021-01-25 13:14:21 -0500

    Added site header block that combines current date and a search field.

---
+++ src/EventSubscriber/Theme/ThemeOmnipediaHeaderEventSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\omnipedia_block\EventSubscriber\Theme;
+
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\core_event_dispatcher\Event\Theme\ThemeEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Omnipedia site header hook_theme() event subscriber.
+ */
+class ThemeOmnipediaHeaderEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The Drupal module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Event subscriber constructor; saves dependencies.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The Drupal module handler service.
+   */
+  public function __construct(
+    ModuleHandlerInterface $moduleHandler
+  ) {
+    $this->moduleHandler = $moduleHandler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::THEME => 'onTheme',
+    ];
+  }
+
+  /**
+   * Defines the Omnipedia site header theme element.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Theme\ThemeEvent $event
+   *   The event object.
+   */
+  public function onTheme(ThemeEvent $event): void {
+    $event->addNewTheme('omnipedia_header', [
+      'variables' => [
+        'current_date'  => [],
+        'search_form'   => [],
+      ],
+      'template'  => 'omnipedia-header',
+      // Path is required.
+      //
+      // @see https://www.drupal.org/project/hook_event_dispatcher/issues/3038311
+      'path'      => $this->moduleHandler
+        ->getModule('omnipedia_block')->getPath() . '/templates',
+    ]);
+  }
+
+}
---
+++ src/Plugin/Block/Header.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Drupal\omnipedia_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Template\Attribute;
+use Drupal\omnipedia_core\Service\TimelineInterface;
+use Drupal\omnipedia_search\Service\WikiSearchInterface;
+use Drupal\views\ViewExecutableFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Site header block.
+ *
+ * This displays the current Omnipedia date and a search field.
+ *
+ * @Block(
+ *   id           = "omnipedia_header",
+ *   admin_label  = @Translation("Header"),
+ *   category     = @Translation("Omnipedia"),
+ * )
+ */
+class Header extends BlockBase implements BlockPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * The Views executable factory.
+   *
+   * @var \Drupal\views\ViewExecutableFactory
+   */
+  protected $viewsExecutableFactory;
+
+  /**
+   * The Views entity storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $viewsStorage;
+
+  /**
+   * The Omnipedia timeline service.
+   *
+   * @var \Drupal\omnipedia_core\Service\TimelineInterface
+   */
+  protected $timeline;
+
+  /**
+   * The Omnipedia wiki search service.
+   *
+   * @var \Drupal\omnipedia_search\Service\WikiSearchInterface
+   */
+  protected $wikiSearch;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\views\ViewExecutableFactory $viewsExecutableFactory
+   *   The Views executable factory.
+   *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $viewsEntityStorage
+   *   The Views entity storage.
+   *
+   * @param \Drupal\omnipedia_core\Service\TimelineInterface $timeline
+   *   The Omnipedia timeline service.
+   *
+   * @param \Drupal\omnipedia_search\Service\WikiSearchInterface $wikiSearch
+   *   The Omnipedia wiki search service.
+   */
+  public function __construct(
+    array $configuration, string $pluginID, array $pluginDefinition,
+    TimelineInterface       $timeline,
+    EntityStorageInterface  $viewsEntityStorage,
+    ViewExecutableFactory   $viewsExecutableFactory,
+    WikiSearchInterface     $wikiSearch
+  ) {
+    parent::__construct($configuration, $pluginID, $pluginDefinition);
+
+    // Save dependencies.
+    $this->timeline               = $timeline;
+    $this->viewsEntityStorage     = $viewsEntityStorage;
+    $this->viewsExecutableFactory = $viewsExecutableFactory;
+    $this->wikiSearch             = $wikiSearch;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration, $pluginID, $pluginDefinition
+  ) {
+    return new static(
+      $configuration, $pluginID, $pluginDefinition,
+      $container->get('omnipedia.timeline'),
+      $container->get('entity_type.manager')->getStorage('view'),
+      $container->get('views.executable'),
+      $container->get('omnipedia.wiki_search')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMachineNameSuggestion() {
+    return 'omnipedia_header';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $renderArray = [
+      'header'  => [
+        '#theme'  => 'omnipedia_header',
+
+        '#current_date'    => [
+          '#type'         => 'html_tag',
+          '#tag'          => 'time',
+          '#attributes'   => new Attribute([
+            'class'         => [
+              'omnipedia-current-date',
+            ],
+            'datetime'      => $this->timeline
+              ->getDateFormatted('current', 'html'),
+          ]),
+          '#value'        => $this->timeline
+            ->getDateFormatted('current', 'long'),
+        ],
+      ],
+
+      '#cache'      => [
+        // This gets cached permanently, varying by the storage-formatted date,
+        // i.e. different cache context for each date, and by whether the
+        // current route is a wiki search page.
+        'contexts'    => ['omnipedia_dates', 'omnipedia_is_wiki_search_page'],
+        'tags'        => [
+          'omnipedia_dates:' . $this->timeline
+            ->getDateFormatted('current', 'storage')
+        ],
+        'max-age'     => Cache::PERMANENT,
+      ],
+    ];
+
+    /** @var array */
+    $searchForm = $this->getSearchForm();
+
+    if (!empty($searchForm)) {
+      if (isset($searchForm['#attributes'])) {
+        $searchForm['#attributes'] = new Attribute($searchForm['#attributes']);
+
+      } else {
+        $searchForm['#attributes'] = new Attribute();
+      }
+
+      $renderArray['header']['#search_form'] = $searchForm;
+    }
+
+    return $renderArray;
+  }
+
+  /**
+   * Get the wiki search form.
+   *
+   * @return array
+   *   The form render array or an empty array on error.
+   *
+   * @see \Drupal\views\Views::getView()
+   *   We load the view like in this static method except with dependency
+   *   injection.
+   */
+  protected function getSearchForm(): array {
+    // Don't display the search form on the wiki search page as it's redundant.
+    if ($this->wikiSearch->isCurrentRouteSearchPage()) {
+      return [];
+    }
+
+    /** @var \Drupal\views\ViewExecutable|null */
+    $viewEntity = $this->viewsEntityStorage->load('wiki_search');
+
+    if (!\is_object($viewEntity)) {
+      return [];
+    }
+
+    /** @var \Drupal\views\ViewExecutable */
+    $viewExecutable = $this->viewsExecutableFactory->get($viewEntity);
+
+    // We have to build the display to ensure that various handlers are
+    // initialized so that we don't cause any errors when building the form.
+    $viewExecutable->build('page');
+
+    return $viewExecutable->getDisplay()->getPlugin('exposed_form')
+      ->renderExposedForm(true);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(
+      parent::getCacheContexts(),
+      ['omnipedia_dates', 'omnipedia_is_wiki_search_page']
+    );
+  }
+
+}
