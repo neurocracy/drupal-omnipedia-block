04fed3d3df78c77977f4d77529e85400c84603c0
Matei Stanca <i@ambientimpact.com>
2022-11-03 18:12:16 -0400

    Converted Webpack config to Symfony Webpack Encore.

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,12 @@
 
 const autoprefixer = require('autoprefixer');
 const componentPaths = require('ambientimpact-drupal-modules/componentPaths');
+const Encore = require('@symfony/webpack-encore');
 const glob = require('glob');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const SourceMapDevToolPlugin = require('webpack/lib/SourceMapDevToolPlugin');
-
-const isDev = (process.env.NODE_ENV !== 'production');
 
 const distPath = '.webpack-dist';
 
@@ -52,125 +50,78 @@ function getGlobbedEntries() {
 
 };
 
-/**
- * Array of plug-in instances to pass to Webpack.
- *
- * @type {Array}
- */
-let plugins = [
-  new RemoveEmptyScriptsPlugin(),
-  new MiniCssExtractPlugin(),
-];
-
-if (isDev === true) {
-  plugins.push(
-    new SourceMapDevToolPlugin({
-      filename: '[file].map',
-    })
-  );
+// @see https://symfony.com/doc/current/frontend/encore/installation.html#creating-the-webpack-config-js-file
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-module.exports = {
+Encore
+.setOutputPath(path.resolve(__dirname, (outputToSourcePaths ? '.' : distPath)))
+// Encore will complain if the public path doesn't start with a slash.
+// Unfortunately, it doesn't seem Webpack's automatic public path works here.
+//
+// @see https://webpack.js.org/guides/public-path/#automatic-publicpath
+.setPublicPath('/')
+.setManifestKeyPrefix('')
+.disableSingleRuntimeChunk()
+.configureFilenames({
+  js:     'temp/[name].js',
+  assets: '[file][query]',
+})
+.addEntries(getGlobbedEntries())
+.enableSourceMaps(!Encore.isProduction())
 
-  mode:     isDev ? 'development' : 'production',
-  devtool:  isDev ? 'eval-cheap-module-source-map': false,
-
-  entry: getGlobbedEntries,
-
-  plugins: plugins,
-
-  output: {
-
-    path: path.resolve(__dirname, (outputToSourcePaths ? '.' : distPath)),
-
-    // Be very careful with this - if outputting to the source paths, this must
-    // not be true or it'll delete everything contained in the directory without
-    // warning.
-    clean: !outputToSourcePaths,
-
-    // Since Webpack started out primarily for building JavaScript applications,
-    // it always outputs a JS files, even if empty. We place these in a
-    // temporary directory by default.
-    filename: 'temp/[name].js',
-
-    // Assets are left at their original locations and not hashed. The [query]
-    // must be left in to ensure any query string specified in the CSS is
-    // preserved.
-    //
-    // @see https://stackoverflow.com/questions/68737296/disable-asset-bundling-in-webpack-5#68768905
-    //
-    // @see https://github.com/webpack-contrib/css-loader/issues/889#issuecomment-1298907914
-    assetModuleFilename: '[file][query]',
-
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
+// We don't use Babel so we can probably just remove all presets to speed it up.
+//
+// @see https://github.com/symfony/webpack-encore/issues/154#issuecomment-361277968
+.configureBabel(function(config) {
+  config.presets = [];
+})
+.addPlugin(new RemoveEmptyScriptsPlugin())
+.enableSassLoader(function(options) {
+  options.sassOptions = {includePaths: componentPaths().all};
+})
+.enablePostCssLoader(function(options) {
+  options.postcssOptions = {
+    plugins: [
+      autoprefixer(),
+    ],
+  };
+})
+// Re-enable automatic public path for paths referenced in CSS.
+//
+// @see https://github.com/symfony/webpack-encore/issues/915#issuecomment-1189319882
+.configureMiniCssExtractPlugin(function(config) {
+  config.publicPath = 'auto';
+})
+// Disable the Encore image rule because we provide our own loader config.
+.configureImageRule({enabled: false})
+.addLoader({
+  test: /\.(jpe?g|png)$/i,
+  loader: ImageMinimizerPlugin.loader,
+  enforce: 'pre',
+  options: {
+    generator: [{
+      preset: 'webp',
+      implementation: ImageMinimizerPlugin.sharpGenerate,
+      options: {
+        plugins: ['sharp-webp'],
+        encodeOptions: {
+          webp: {
+            quality: 80,
           },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: isDev,
-              importLoaders: 2,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: isDev,
-              postcssOptions: {
-                plugins: [
-                  autoprefixer(),
-                ],
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDev,
-              sassOptions: {
-                includePaths: componentPaths().all,
-              }
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|png)$/i,
-        loader: ImageMinimizerPlugin.loader,
-        enforce: 'pre',
-        options: {
-          generator: [
-            {
-              preset: 'webp',
-              implementation: ImageMinimizerPlugin.sharpGenerate,
-              options: {
-                plugins: ['sharp-webp'],
-                encodeOptions: {
-                  webp: {
-                    quality: 80,
-                  },
-                },
-              },
-              // Annoyingly, file URLs that are altered (e.g. PNG to WebP) by
-              // this loader appear to incorrectly generate paths using the
-              // platform's path separator. This means that if built on Windows,
-              // the URLs will use a backslash (\), which is not a path
-              // separator in an HTTP URL but rather an escape character,
-              // meaning that the URL will be incorrect and a 404.
-              filename: function(file) {
-                return file.filename.replaceAll('\\', '/');
-              },
-            },
-          ],
         },
       },
-    ],
+      // Annoyingly, file URLs that are altered (e.g. PNG to WebP) by this
+      // loader appear to incorrectly generate paths using the platform's path
+      // separator. This means that if built on Windows, the URLs will use a
+      // backslash (\), which is not a path separator in an HTTP URL but rather
+      // an escape character, meaning that the URL will be incorrect and a 404.
+      filename: function(file) {
+        return file.filename.replaceAll('\\', '/');
+      },
+    }],
   },
-};
+});
+
+module.exports = Encore.getWebpackConfig();
