651694ead5f2ca611a40647642b2b7fe030598ff
Matei Stanca <i@ambientimpact.com>
2023-11-20 15:50:46 -0500

    Converted Webpack config to ESM.

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,12 +1,20 @@
-'use strict';
+import Encore from '@symfony/webpack-encore';
+// import { glob } from 'glob'; // @todo Upgrade to glob 10.x which has ESM.
+import * as path from 'node:path';
 
-const autoprefixer = require('autoprefixer');
-const componentPaths = require('drupal-ambientimpact-core/componentPaths');
-const Encore = require('@symfony/webpack-encore');
-const glob = require('glob');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const path = require('path');
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+// The remaining modules are CommonJS only. Because of this, they must be
+// import()ed and destructured like so to behave similarly to ESM imports.
+const { default: autoprefixer } = await import('autoprefixer');
+const { default: componentPaths } = await import(
+  'drupal-ambientimpact-core/componentPaths',
+);
+const { default: glob } = await import('glob');
+const { default: ImageMinimizerPlugin } = await import(
+  'image-minimizer-webpack-plugin',
+);
+const { default: RemoveEmptyScriptsPlugin } = await import(
+  'webpack-remove-empty-scripts',
+);
 
 const distPath = '.webpack-dist';
 
@@ -54,8 +62,10 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-Encore
-.setOutputPath(path.resolve(__dirname, (outputToSourcePaths ? '.' : distPath)))
+Encore.setOutputPath(path.resolve(
+  path.dirname(new URL(import.meta.url).pathname),
+  (outputToSourcePaths ? '.' : distPath)
+))
 
 // Encore will complain if the public path doesn't start with a slash.
 // Unfortunately, it doesn't seem Webpack's automatic public path works here.
@@ -90,6 +100,8 @@ Encore
 
 // Clean out any previously built files in case of source files being removed or
 // renamed.
+//
+// @see https://github.com/johnagan/clean-webpack-plugin
 .cleanupOutputBeforeBuild(['**/*.css', '**/*.css.map'])
 
 .enableSourceMaps(!Encore.isProduction())
@@ -106,7 +118,9 @@ Encore
 .addPlugin(new RemoveEmptyScriptsPlugin())
 
 .enableSassLoader(function(options) {
-  options.sassOptions = {includePaths: componentPaths().all};
+  options.sassOptions = {
+    includePaths: componentPaths().all,
+  };
 })
 .enablePostCssLoader(function(options) {
   options.postcssOptions = {
@@ -163,4 +177,4 @@ Encore
   },
 });
 
-module.exports = Encore.getWebpackConfig();
+export default Encore.getWebpackConfig();
